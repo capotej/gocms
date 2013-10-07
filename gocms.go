0f07b2c9ef2040d7ca66f40e3e675521b02512e5
Julio Capote <julio@twitter.com>
2013-10-06 18:06:35 -0700

    transforms

--- gocms.go
+++ gocms.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	//"code.google.com/p/go-html-transform/html/transform"
+	// "code.google.com/p/go-html-transform/css/selector"
+	"bytes"
+	"code.google.com/p/go-html-transform/h5"
+	"code.google.com/p/go-html-transform/html/transform"
+	"code.google.com/p/go.net/html"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,8 +14,9 @@ import (
 func main() {
 	var templateFile = flag.String("template", "layout.html", "Template file")
 	var inputFile = flag.String("input", "/dev/stdin", "Input file")
+	var outputFile = flag.String("output", "/dev/stdout", "Output file")
 	flag.Parse()
-	// var outputFile = flag.String("output", "/dev/stdout", "Output file")
+
 	templateBytes, err := ioutil.ReadFile(*templateFile)
 	if err != nil {
 		panic(err)
@@ -22,11 +27,28 @@ func main() {
 		panic(err)
 	}
 
-	templateDocStr := string(templateBytes)
-	inputDocStr := string(inputBytes)
+	inputReader := bytes.NewReader(inputBytes)
+	inputTree, _ := h5.New(inputReader)
 
-	fmt.Println(inputDocStr)
-	fmt.Println("**************")
-	fmt.Println(templateDocStr)
+	templateReader := bytes.NewReader(templateBytes)
+	templateTree, _ := h5.New(templateReader)
+	templateTransform := transform.New(templateTree)
 
+	fmt.Printf("%#v\n", templateTransform)
+
+	inputTree.Walk(func(n *html.Node) {
+		if n.Data == "div" {
+			// templateTransform.Apply(Replace(Text("my new text"), "a")
+			fmt.Printf("%#v\n", n)
+		}
+	})
+
+	err = ioutil.WriteFile(*outputFile, inputBytes, 0644)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(*outputFile, templateBytes, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
